cc6abd2d80f1ebb51b9a4a2315b4ca3b8285726c
Pavel <sterallion@gmail.com>
2018-06-28 17:04:48 +1000

    vue cli

---
+++ app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <main>
+      <tetris></tetris>
+    </main>
+    <div class="overlay"></div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+  import Tetris from './components/Tetris.vue';
+
+  @Component({
+    components: {
+      Tetris
+    },
+  })
+  export default class App extends Vue {
+  }
+</script>
+
+<style src="./app.scss" lang="scss"></style>
